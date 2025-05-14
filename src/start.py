a2108c9e99a405591cfe90749c3c402e60678163
ᴋᴜɴᴀʟ <168260488+kxunal@users.noreply.github.com>
2025-05-14 08:38:45 +0530

    Update start.py

--- src/start.py
+++ src/start.py
@@ -1,18 +1,19 @@
 from pyrogram import filters
-from pyrogram import Client
-from helper.data import HACK_MODS,HACK_TEXT
 from pyrogram.types import CallbackQuery
-from bot import app
+from helper.data import HACK_MODS, HACK_TEXT
+from bot import app 
 
- 
 @app.on_message(filters.command("hack") & filters.private)
 async def _hack(_, message):
-    await message.reply_text(HACK_TEXT,
-              reply_markup = HACK_MODS) 
-
+    await message.reply_text(
+        HACK_TEXT,
+        reply_markup=HACK_MODS
+    )
 
 @app.on_callback_query(filters.regex("hack_btn"))
-async def heck_callback(bot : app, query : CallbackQuery):
+async def heck_callback(_, query: CallbackQuery):
     await query.message.delete()
-    await query.message.reply_text(HACK_TEXT,
-              reply_markup = HACK_MODS) 
+    await query.message.reply_text(
+        HACK_TEXT,
+        reply_markup=HACK_MODS
+    )
